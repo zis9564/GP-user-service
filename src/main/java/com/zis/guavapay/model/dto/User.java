85505a012b94f024f211d31fd397b8d873e029e8
zis9564 <zis9564@gmail.com>
2022-11-17 14:46:38 +0300

    implemented triggers for db

--- src/main/java/com/zis/guavapay/model/dto/User.java
+++ src/main/java/com/zis/guavapay/model/dto/User.java
@@ -1,29 +1,36 @@
 package com.zis.guavapay.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.avro.reflect.Nullable;
+
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
+    @Nullable
     @JsonProperty("id")
-    private Long id;
+    private UUID id;
     @JsonProperty("name")
     private String name;
     @JsonProperty("lastname")
     private String lastname;
     @JsonProperty("role")
-    private Long role;
+    private UUID role;
     @JsonProperty("phone")
     private String phone;
 
     @JsonCreator
-    public User(@JsonProperty("id")Long id,
+    public User(
+                @Nullable @JsonProperty("id")UUID id,
                 @JsonProperty("name")String name,
                 @JsonProperty("lastname")String lastname,
-                @JsonProperty("role")Long role,
+                @JsonProperty("role")UUID role,
                 @JsonProperty("phone")String phone) {
         this.id = id;
         this.name = name;
